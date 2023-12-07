8ed55bb10217c0633a1a757336deeb1482b660a9
danalger <mal_kav@yahoo.com>
2023-12-07 12:13:37 -0500

    updated comments in code on line 10 and 23

--- main.py
+++ main.py
@@ -7,6 +7,7 @@
 # Above this line is original code for project where everything is hard coded
 # 
 # Below this line is code to use the program with variables
+# Code does not verify that the file read is a txt string at the moment.  Code will error if other types of files are inputted
 def main():
     book_path = "books/frankenstein.txt" #book to be read needs to be in the books directory where the file is run
                                          #to change read book change frankenstein.txt to other book.txt
@@ -19,8 +20,8 @@ def get_book_text(path):
     with open(path) as f: # path is set to book_path in main
         return f.read()
 
-def get_num_words(book_string):
-    words = book_string.split()
+def get_num_words(book_string): # book_string is set to text in main this is the data read from the path as a text string
+    words = book_string.split() 
     return len(words)
 
 main()
